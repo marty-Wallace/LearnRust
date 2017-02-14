17455cef84eab911a8c868cfc76fab79b9652a5d
Martin Wallace <martin.v.wallace@ieee.org>
2017-02-13 23:15:54 -0800

    Updated main to print errs to stderr instead of stdout

--- grepr/src/main.rs
+++ grepr/src/main.rs
@@ -2,20 +2,25 @@ extern crate grepr;
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 
 use grepr::Config;
 
 fn main() {
+    let mut stderr = std::io::stderr();
     let args: Vec<String> = env::args().collect();
 
     let config  = Config::new(&args)
                          .unwrap_or_else(|err| {
-                            println!("Problem parsing arguments: {}", err);
-                            process::exit(1);
+                                writeln!(&mut stderr, "Problem parsing arguments: {}", err)
+                                        .expect("Could not write to stderr");
+
+                                process::exit(1);
                          });
 
     if let Err(e) = grepr::run(config) {
-        println!("Application error: {}", e);
+        writeln!(&mut stderr, "Application error: {}", e)
+                .expect("Could not write to stderr");
         process::exit(1);
     }
     
