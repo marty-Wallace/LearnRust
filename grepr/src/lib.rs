8a0339fe4efffc26997eafb98116170037998564
Martin Wallace <martin.v.wallace@ieee.org>
2017-02-13 22:33:20 -0800

    Halfway through grepr tutorial

---
+++ grepr/src/lib.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+
+pub struct Config {
+    pub search: String,
+    pub filename: String,
+}
+
+impl Config {
+
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let search = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {
+            search: search, 
+            filename: filename,
+        })
+    }
+}
+
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut file = File::open(config.filename).expect("File not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Something went wrong reading the file");
+
+    println!("With text \n{}", contents);
+
+    Ok(())
+}
