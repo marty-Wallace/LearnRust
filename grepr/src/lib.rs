5ad0b09e1882b98ca66c51d915292966dc8b5250
Martin Wallace <martin.v.wallace@ieee.org>
2017-02-13 23:09:29 -0800

    Added a case insensitive search turned on with environment variable

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+use std::env;
+
 
 pub fn grep<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
@@ -13,9 +15,21 @@ pub fn grep<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn grep_case_insensitive<'a>(search: &str, contents: &'a str) -> Vec<&'a str> {
+    let search = search.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&search) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub struct Config {
     pub search: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -27,9 +41,18 @@ impl Config {
         let search = args[1].clone();
         let filename = args[2].clone();
 
+        let mut case_sensitive = true;
+        for (name,_) in env::vars() {
+            if name == "CASE_INSENSITIVE" {
+                case_sensitive = false;
+                break;
+            }
+        }
+
         Ok(Config {
             search: search, 
             filename: filename,
+            case_sensitive: case_sensitive,
         })
     }
 }
@@ -40,8 +63,10 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Something went wrong reading the file");
+    
+    let f_grep = if config.case_sensitive { grep } else { grep_case_insensitive };
 
-    for line in grep(&config.search, &contents){
+    for line in f_grep(&config.search, &contents){
         println!("{}", line);
     }
 
@@ -51,13 +76,21 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 #[cfg(test)]
 mod test {
     use grep;
+    use grep_case_insensitive;
 
     #[test]
     fn one_result() {
         let search = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
-    
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
         assert_eq!(vec!["safe, fast, productive."], grep(search, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let search = "rust";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+        
+        assert_eq!(vec!["Rust:", "Trust me."], grep_case_insensitive(search, contents));
     }
 
 }
